09a283934868729b08a130e5955106f400c5329d
Олег Барнев <obarnev@inbox.ru>
2022-10-03 20:13:29 +0300

    procfile, db, alembic to deploy

--- db.py
+++ db.py
@@ -8,8 +8,8 @@ from sqlalchemy.orm import sessionmaker
 
 
 # DATABASE_URL = postgresql+asyncpg://CourseHunter:CourseHunter@db:5432/CourseHunter
-DATABASE_URL = os.environ.get("DATABASE_URL")
-
+# DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASE_URL = "postgres://lzymozdotgazte:dcd435e4b52bb8ccb1fdf85766c83d870a5488160be3881e30212547aa4c7cc6@ec2-54-228-30-162.eu-west-1.compute.amazonaws.com:5432/d3lotjev0237d1"
 engine = create_async_engine(DATABASE_URL, echo=True, future=True)
 
 AsyncSessionLocal = sessionmaker(
