cf219a4b15e0eeaa440eb37988284ce4729f35ea
AnthonyRedGrave <69057540+AnthonyRedGrave@users.noreply.github.com>
2022-10-03 21:16:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,21 @@ from fastapi import FastAPI
 
 from users.models import User
 from users.views import users_router
+from fastapi.middleware.cors import CORSMiddleware
 # from db import init_db
+
+origins = ["*"]
+
 
 app = FastAPI()
 app.include_router(users_router, prefix='/api', tags=['users'])
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # CORSHEADERS FOR FRONTEND
